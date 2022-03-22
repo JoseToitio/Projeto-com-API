4a102a47c8f18faf7901a8728357463f9930efff
Toitio <josetoitio@gmail.com>
2022-03-22 19:08:21 -0300

    tentando o requisito 4

--- Middlewares/ageValidate.js
+++ Middlewares/ageValidate.js
@@ -1,11 +1,10 @@
 const ageValidate = (res, req, next) => {
   const { age } = req.body;
-  if (age === undefined || age === '') {
-    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  }
-  if (age < 18) {
-    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
-  }
+  if (age === undefined 
+    || age === '') return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  
+  if (age 
+    < 18) return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' }); 
   next();
 };
 
